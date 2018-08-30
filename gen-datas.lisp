6a90a4c045336a3a0bb116d3838a6295cf24eda6
privet-kitty <privet.kitty99@gmail.com>
2018-08-31 00:37:53 +0200

    Use GridBagLayout

--- gen-datas.lisp
+++ gen-datas.lisp
@@ -1,29 +1,27 @@
 (in-package :cl-user)
 
 (require :dufy)
-(require :dufy-tools)
+(require :mid)
 
 (defparameter current-path *load-pathname*)
-(defparameter srgbd50 (dufy:new-rgbspace 0.64d0 0.33d0 0.3d0 0.6d0 0.15d0 0.06d0
-					 :illuminant dufy:illum-d50))
-(defparameter adobed50 (dufy:new-rgbspace 0.64d0 0.33d0 0.21d0 0.71d0 0.15d0 0.06d0
-					  :illuminant dufy:illum-d50))
 
 (defparameter task-lst
   (list (cons "srgb-d65" dufy:srgb)
-	(cons "srgb-d50" srgbd50)
+	(cons "srgb-d50" dufy:srgbd50)
 	(cons "adobergb-d65" dufy:adobe)
-	(cons "adobergb-d50" adobed50)))
+	(cons "adobergb-d50" dufy:adobed50)))
 
 (defun do-task (basename rgbspace)
   (let* ((filename (concatenate 'string basename ".dat"))
 	 (path (merge-pathnames filename current-path)))
     (format t "Now generating ~A...~%" filename)
-    (dufy-tools:save-munsell-inversion-data
-     (dufy-tools::make-munsell-inversion-data rgbspace t)
+    (mid:save-munsell-inversion-data
+     (mid::make-munsell-inversion-data rgbspace t)
      path)))
   
 ;; (dolist (task task-lst)
 ;;   (do-task (car task) (cdr task))
 ;;   ))
 
+
+;; (defparameter mid (mid:load-munsell-inversion-data (merge-pathnames "srgb-d65.dat" current-path)))
